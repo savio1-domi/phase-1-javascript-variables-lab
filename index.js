afc9be597df9d1997f74cb22ed4cea97092e72fb
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-20 12:57:29 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solution in this file!
-let companyName = 'Scuber' ; {
-    const companyName = 'Scuber'
-}
+const companyName = 'Scuber'
 
 let mostProfitableNeighborhood = 'Chelsea'
 
